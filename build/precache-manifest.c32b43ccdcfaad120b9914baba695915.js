c8a08e80803f53cd3ccc255b85212e7a60d55135
MrWhite1338 <aleksa.marjanov@outlook.com>
2022-04-23 10:30:17 -0500

    fixed image logo

---
+++ build/precache-manifest.c32b43ccdcfaad120b9914baba695915.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76c03bc4c7b6328ca8eaa202fbdeca8a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f08fe15397d36c5458a9",
+    "url": "/static/js/2.91911c25.chunk.js"
+  },
+  {
+    "revision": "513e47d94288488bd0be",
+    "url": "/static/js/main.8ef9db08.chunk.js"
+  },
+  {
+    "revision": "a205d16c54de8ede2d88",
+    "url": "/static/js/runtime-main.0bafec10.js"
+  }
+]);
